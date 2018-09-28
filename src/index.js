a722d15dbb57a1f0e949c8bb866431a889cf0675
Nikhil <nikhilabcdenk@gmail.com>
2018-09-29 03:19:36 +0530

    Added Time travel feature (allows to go to any previous move)

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  for(let i = 1; i<lines.length; i++){
+  for(let i = 0; i<lines.length; i++){
     const [a,b, c] = lines[i];
     if( squares[a] && squares[b] === squares[a] && squares[c] === squares[a] ){
       return squares[a];
@@ -34,53 +34,16 @@ function calculateWinner(squares) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if(calculateWinner(squares) || squares[i]){
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({ 
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   renderSquare(i) {
     return( <Square 
-    value={this.state.squares[i]}
-    onClick={() => this.handleClick(i)} />
+    value={this.props.squares[i]}
+    onClick={() => this.props.onClick(i)} />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    let isDraw = false;
-    for(let i = 0; i < 9; i++){
-      if(!this.state.squares[i]){
-        isDraw = true;
-      }
-    }
-    if(!isDraw) {
-      status = "Tie Game";
-    } else if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -102,15 +65,78 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    }
+  }
+  
+  handleClick(i) {
+    const history = this.state.history.slice(0,this.state.stepNumber+1);
+    const current = history[history.length-1];
+    const squares = current.squares.slice();
+    if(calculateWinner(squares) || squares[i]){
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ 
+      history: history.concat([{
+        squares: squares,
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step%2) === 0,
+    })
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    let status;
+    let isDraw = false;
+    for(let i = 0; i < 9; i++){
+      if(!current.squares[i]){
+        isDraw = true;
+      }
+    }
+    if(!isDraw) {
+      status = "Tie Game";
+    } else if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+      'Go to move #' + move :
+      'Go to game Start';
+      return(
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
